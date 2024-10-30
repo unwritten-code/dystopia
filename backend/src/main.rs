ea9eb477105f7707ca45924e9e36be11637509e5
ayephillip <phillip@unwritten.co>
2024-10-30 10:50:39 +0000

    replaced xlsxwriter with umya-spreadsheet as incompatible

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,7 @@
 use axum::{routing::get, Router};
 
 async fn hello_world() -> &'static str {
-    "Hello, world! Xlsxwriter compiled"
+    "Hello, world! umya-spreadsheet compiled"
 }
 
 #[shuttle_runtime::main]
