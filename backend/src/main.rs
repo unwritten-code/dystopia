f332aae9f660ad3909156d43c27594f3e609fbce
ayephillip <phillip@unwritten.co>
2024-10-31 13:53:37 +0000

    download excel locally

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,33 +3,35 @@ use axum::{
     http::{header, HeaderValue, StatusCode},
     response::IntoResponse,
     routing::get,
+    body::Body,
     Router,
 };
 
-use umya_spreadsheet::*;
+// use umya_spreadsheet::*;
 
-async fn excel_generator(Path(val): Path<String>) -> impl IntoResponse {
-    // format!("Hello, world name: {value}! umya-spreadsheet compiled!");
+use bytes::Bytes;
+
+async fn excel_generator(Path(val): Path<String>) -> impl IntoResponse {    
     // setup spreadsheet
-    let mut book = new_file();
-    let _ = book.new_sheet("Unwritten");
+    // let mut book = new_file();
+    // let _ = book.new_sheet("Unwritten");
 
-    // Insert `val` into a specific cell in the "Unwritten" sheet
-    book.get_sheet_by_name_mut("Unwritten")
-        .unwrap()
-        .get_cell_mut("B2") // cell reference "B2"
-        .set_value(val.clone());
+    // insert parse `val` into spreadsheet
+    // book.get_sheet_by_name_mut("Unwritten")
+    //     .unwrap()
+    //     .get_cell_mut("B2") // cell reference
+    //     .set_value(val.clone());
+
+    let file_data = Bytes::from_static(include_bytes!(".././phillip-sucks.xlsx"));
 
 
-    // store in a memory buffer, excel is binary so store as u8
-    let buffer: Vec<u8> = Vec::new();
-
+    
     // setup headers for a downloadable file
      let mut headers = axum::http::HeaderMap::new();
      headers.insert(header::CONTENT_TYPE, HeaderValue::from_static("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
      headers.insert(header::CONTENT_DISPOSITION, HeaderValue::from_str("attachment; filename=\"model.xlsx\"").unwrap());
 
-     (StatusCode::OK, headers, buffer)
+     (StatusCode::OK, headers, Body::from(file_data))
 }
 
 
@@ -39,3 +41,7 @@ async fn main() -> shuttle_axum::ShuttleAxum {
     
     Ok(router.into())
 }
+
+// Create a temporary path
+    // let path = std::path::Path::new("./model3.xslx");
+    // let _ = writer::xlsx::write(&book, path);
