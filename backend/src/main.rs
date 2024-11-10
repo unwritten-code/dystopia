e2b9cc45361ec5587464a7abaa835a4faab058fe
ayephillip <phillip@unwritten.co>
2024-11-10 16:10:50 +0000

    rename to polars_end_point

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -16,7 +16,7 @@ mod models{
 }
 
 
-async fn send_it(Json(inputs): Json<Value>) -> impl IntoResponse {
+async fn polars_end_point(Json(inputs): Json<Value>) -> impl IntoResponse {
 
     // Convert JSON data to a string and wrap it in a Cursor
     let json_data = serde_json::to_string(&inputs).expect("Failed to serialize JSON");
@@ -41,12 +41,12 @@ async fn send_it(Json(inputs): Json<Value>) -> impl IntoResponse {
 async fn main() -> ShuttleAxum {
 
     /*
-    curl http://127.0.0.1:8000/send_it/ \
+    curl http://127.0.0.1:8000/polars_end_point/ \
     -H "Content-Type: application/json" \
     -d '{"company_name": "ACME Corp", "primary_sector": "Oil and Gas", "total_revenue": 999, "revenue_by_country": [{ "search_term": "FRA", "value": 0.40, "key": "abc" }, { "search_term": "GBR", "value": 0.60, "key": "def" }]}' 
   */
 
-    let router = Router::new().route("/send_it/", post(send_it));
+    let router = Router::new().route("/polars_end_point/", post(polars_end_point));
 
     Ok(router.into())
 }
